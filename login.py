a6067304096f19fdd6370688eb7f4fc7416255b1
Leyla Mammadova <mammadova19@itu.edu.tr>
2021-10-31 15:46:36 +0300

    Login system added

---
+++ login.py
@@ -0,0 +1,34 @@
+from database import Database
+
+class user_login:
+    def __init__(self, data) -> None:
+        if data == None:
+            self.data = None
+        else:
+            self.data = {
+                "id": data[0],
+                "name": data[1],
+                "last_name": data[2],
+                "gender": data[3],
+                "country": data[4],
+                "birthday": data[5],
+                "email": data[6]
+            }
+
+    @property
+    def is_authenticated(self):
+        return self.data != None
+
+    @property
+    def is_active(self):
+        return True
+    
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.data['id']
+
+def load_user(user_id):
+    return user_login(Database().get_user(user_id,"ID"))
