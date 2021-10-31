d3ac7a9c9af2165fc890a05394a849d37d71409f
Leyla Mammadova <mammadova19@itu.edu.tr>
2021-10-31 15:45:33 +0300

    Database class created

--- database.py
+++ database.py
@@ -1,20 +1,47 @@
 import sqlite3 
-from sqlite3 import Error
 
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        cursor = conn.cursor()
-        cursor.execute("SELECT value FROM total_fertility WHERE (country_iso_code = \"AZE\" AND year = 2000)")
-        value, = cursor.fetchone()
-        print(value)
-        if conn:
-            conn.close()
+class Database:
+    def __init__(self, dbfile = r"pythonsqlite.db"):
+        self.dbfile = dbfile
 
-if __name__ == '__main__':
-    create_connection(r"pythonsqlite.db")
+    def add_user(self, first_name, last_name, gender, country, birthday, email, password_hash):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO users (NAME, LAST_NAME, GENDER, COUNTRY, BIRTHDAY, EMAIL, PASSWORD) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(query, (first_name, last_name, gender, country, birthday, email, password_hash))
+            connection.commit()
+
+    def update_user(self, id, name, last_name, gender, country, birthday, email, password):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE users SET NAME = ?, LAST_NAME = ?, GENDER = ?, COUNTRY = ?, BIRTHDAY = ?, EMAIL = ?, PASSWORD = ? WHERE (ID = ?)"
+            cursor.execute(query, (name, last_name, gender, country, birthday, email, password, id))
+            connection.commit()
+
+    def delete_user(self, id):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM users WHERE (ID = ?)"
+            cursor.execute(query, (id,))
+            connection.commit()
+
+    def get_countries(self):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT DISTINCT country FROM total_fertility"
+            cursor.execute(query)
+        return cursor.fetchall()
+
+    def get_password(self, email):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT PASSWORD FROM users WHERE (EMAIL = ?)"
+            cursor.execute(query, (email,))
+        return cursor.fetchone()
+
+    def get_user(self, value, parameter = "ID"):
+        with sqlite3.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM users WHERE ({} = ?)".format(parameter) 
+            cursor.execute(query, (value,))
+        return cursor.fetchone()
